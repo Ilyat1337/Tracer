d122ada262924de0411dc4289fb9586ac093cc18
Ilyat1337 <miningilya2@gmail.com>
2020-09-14 13:42:22 +0300

    add xml serialization attributes

--- Tracer/ITracer.cs
+++ Tracer/ITracer.cs
@@ -1,13 +1,11 @@
-﻿using System;
-
-namespace Tracer
+﻿namespace Tracer
 {
     public interface ITracer
     {
         // вызывается в начале замеряемого метода
         void StartTrace();
 
-    // вызывается в конце замеряемого метода 
+        // вызывается в конце замеряемого метода 
         void StopTrace();
 
         // получить результаты измерений  
--- Tracer/TraceResult.cs
+++ Tracer/TraceResult.cs
@@ -1,7 +1,9 @@
 ﻿using System.Collections.Generic;
+using System.Xml.Serialization;
 
 namespace Tracer
 {
+    [XmlRoot("root")]
     public class TraceResult
     {
         public TraceResult()
@@ -9,46 +11,64 @@ namespace Tracer
             Threads = new List<ThreadTraceResult>();
         }
 
+        [XmlElement("thread")]
         public List<ThreadTraceResult> Threads
-        { get; }
+        { get; set; }
     }
-    
+
     public class ThreadTraceResult
     {
+        public ThreadTraceResult()
+        {
+
+        }
+
         public ThreadTraceResult(int threadId)
         {
             Id = threadId;
             Methods = new List<MethodTraceResult>();
         }
 
+        [XmlAttribute("id")]
         public int Id
-        { get; }
+        { get; set; }
 
+        [XmlAttribute("time")]
         public int Time
         { get; set; }
 
+        [XmlElement("method")]
         public List<MethodTraceResult> Methods
-        { get; }
+        { get; set; }
 
     }
 
     public class MethodTraceResult
     {
+        public MethodTraceResult()
+        {
+
+        }
+
         public MethodTraceResult(string methodName, string className)
         {
             Name = methodName;
             Class = className;
         }
 
+        [XmlAttribute("name")]
         public string Name
-        { get; }
+        { get; set; }
 
+        [XmlAttribute("class")]
         public string Class
-        { get; }
+        { get; set; }
 
+        [XmlAttribute("time")]
         public int Time
         { get; set; }
 
+        [XmlElement("method")]
         public List<MethodTraceResult> Methods
         { get; set; }
     }
